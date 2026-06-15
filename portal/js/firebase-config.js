0b6168a935f73ffc9e025bb295aeafdde86c177b
IDEA NOV <ideanow-shift@users.noreply.github.com>
2026-06-15 21:21:18 +0900

    Update Firebase web app configuration

--- portal/js/firebase-config.js
+++ portal/js/firebase-config.js
@@ -2,10 +2,13 @@ export const PORTAL_CONFIG = {
   authMode: "firebase",
   gasApiUrl: "https://script.google.com/macros/s/AKfycbwN4OfUZob_rHpbtxs5xjxY2e1GuSYgnQIT28Lu6I3zJMop-3rde_MQJHR8SYUsNhVr/exec",
   firebase: {
-    apiKey: "AIzaSyBJAPJbAG_SdFmJq00dIKh8v4Sd0tI0Vkc",
+    apiKey: "AIzaSyBJAPJbAG_SdFmJqO0dIKh8v4Sd0tI0Vkc",
     authDomain: "idea-nov-group-portal.firebaseapp.com",
     projectId: "idea-nov-group-portal",
-    appId: "1:664629515628:web:1f289c4bf22f2d0c178f49"
+    storageBucket: "idea-nov-group-portal.firebasestorage.app",
+    messagingSenderId: "664629515628",
+    appId: "1:664629515628:web:3684ed10cc62cbdd178f49",
+    measurementId: "G-TQKXXSW803"
   }
 };
 
